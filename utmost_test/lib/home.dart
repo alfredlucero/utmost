6f946e71fdf05b1b5afa8967b870f7e1560d0e2c
Alfred Lucero <alfred.lucero@ucla.edu>
2024-03-27 22:32:54 -0700

    Can sign in, log out, register, and go to user profile screen

--- utmost_test/lib/home.dart
+++ utmost_test/lib/home.dart
@@ -45,7 +45,7 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Image.asset('dash.png'),
+            Image.asset('flutterfire_300x.png'),
             Text(
               'Welcome!',
               style: Theme.of(context).textTheme.displaySmall,
